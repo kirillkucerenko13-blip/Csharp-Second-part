28bd30f3f06c18738c74d704f0711beb849ccf3c
Xr0ud <kirillkucerenko13@gmail.com>
2026-03-25 12:27:07 +0200

    Add files via upload

---
+++ Task__1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task__1
+{
+    internal class Program :TextOperation
+    {
+        static void Main(string[] args)
+        {
+            string InputFile = ("C:\\Users\\kiril\\OneDrive\\Рабочий стол\\textPD21.txt");
+            string OutputFile = ("C:\\Users\\kiril\\OneDrive\\Рабочий стол\\resultPD21.txt");
+
+            ProcessFile(InputFile, OutputFile, new TextOperation().ToUpperCase);
+            ProcessFile(InputFile, OutputFile, CountSimbols);
+            ProcessFile(InputFile, OutputFile, CountWords);
+
+
+        }
+
+
+
+    }
+}
---
+++ Task__1/TextOp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task__1;
+using System.IO;
+
+namespace Task__1
+{
+    public delegate string TextOp(string text);
+    public  class TextOperation
+    {
+        public string ToUpperCase(string text)
+        {
+            return text.ToUpper();
+        }
+
+        public static string CountSimbols(string text)
+        {
+          
+            return $"CountSimbols{text.Length}";
+        }
+
+        public static string CountWords(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return "CountWords0";
+
+            int wordCount = text
+                .Split(new[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Length;
+            return $"CountWords{wordCount}";
+        }
+        public static void ProcessFile(string inputFile, string outputFile, TextOp operation)
+        {
+
+            string text = File.ReadAllText(inputFile);
+
+
+            string result = operation(text);
+
+            using (StreamWriter sw = new StreamWriter(outputFile, append: true))
+            {
+                sw.WriteLine(result);
+            }
+
+        }
+       
+        
+       }
+
+
+}
+
