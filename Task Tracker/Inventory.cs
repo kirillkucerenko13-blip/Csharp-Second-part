ada70be4099a8577a13926f3fdd639062e772698
Xr0ud <kirillkucerenko13@gmail.com>
2026-04-24 13:54:26 +0300

    Add files via upload

---
+++ Task Tracker/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+namespace Task_Tracker
+{
+    public class Inventory { public List<string> Items { get; set; } = new List<string>(); }
+    public class Player { public string Name { get; set; } public Inventory Inventory { get; set; } }
+
+    public static class Inventories
+    {
+        public static void Run()
+        {
+            string jsonWithoutInventory = "{\"Name\":\"Гравець1\"}";
+            var player = JsonSerializer.Deserialize<Player>(jsonWithoutInventory);
+
+            player.Inventory ??= new Inventory();
+            System.Console.WriteLine($"Гравець: {player.Name}, Предметів: {player.Inventory.Items.Count}");
+        }
+    }
+}
---
+++ Task Tracker/Versioning.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+namespace Task_Tracker
+{
+    public class PlayerV2 { public string Name { get; set; } public int Level { get; set; } = 1; }
+
+    public static class Versioning
+    {
+        public static void Run()
+        {
+            string oldJson = "{\"Name\":\"OldPlayer\"}";
+            var player = JsonSerializer.Deserialize<PlayerV2>(oldJson);
+            System.Console.WriteLine($"Ім'я: {player.Name}, Рівень за замовчуванням: {player.Level}");
+        }
+    }
+}
